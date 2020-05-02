80810b2059135b38bda692e61f76e3e4e28b8eff
Bryce B <bryce.boddy@gmail.com>
2020-05-02 05:42:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @status == complete && @receiver.balance > @amount
+    if @status == "complete" && @receiver.balance > @amount
       @receiver.balance -= @amount
       @sender.balance += @amount
     else
