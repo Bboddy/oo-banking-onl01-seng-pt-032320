3fdc0a4d5b7183696fdb6fa88d9a77cc57af3e3d
Bryce B <bryce.boddy@gmail.com>
2020-05-02 05:39:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,12 +21,12 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      self.status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
   
   def reverse_transfer
-    if
+    if 
   end
 end
